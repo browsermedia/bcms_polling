900364fb270c39bc9818614529352738091540a4
peakpg <peakpg@gmail.com>
2011-02-17 15:03:43 -0500

    Deleting existing polling answers now works.

--- test/unit/models/poll_response_test.rb
+++ test/unit/models/poll_response_test.rb
@@ -3,7 +3,7 @@ require File.join(File.dirname(__FILE__), '/../../test_helper')
 class PollResponseTest < ActiveSupport::TestCase
 
   test "should be able to create new block" do
-    assert PollResponse.create!
+    assert PollResponse.create!(:answer=>"Non-blank")
   end
   
 end
